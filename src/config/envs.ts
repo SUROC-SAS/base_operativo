f900169a10bc7b63cc2ef7a5cab4f88b1cde908c
Yohanny Lugo <78890742+dev-yohannylugo@users.noreply.github.com>
2024-01-26 09:47:37 -0500

    Update envs.ts

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,7 +4,7 @@ import { get } from 'env-var';
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   DB_USERNAME: get('DB_USERNAME').required().asString(),
-  DB_PASSWORD: get('DB_PASSWORD').asString(),
+  DB_PASSWORD: get('DB_PASSWORD').required().asString(),
   DB_NAME: get('DB_NAME').required().asString(),
   DB_HOSTNAME: get('DB_HOSTNAME').required().asString(),
   DB_PORT: get('DB_PORT').required().asPortNumber(),
